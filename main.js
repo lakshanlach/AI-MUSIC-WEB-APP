5f0770991d896cef21900f2b11e02294c04b3df1
lakshanlach <97869720+lakshanlach@users.noreply.github.com>
2022-04-22 11:20:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+leftmusic = ""
+rightmusic = ""
+status = ""
+
+leftwristscore = 0;
+rightwristscore = 0;
+
+status1 = "";
+status2 = "";
+
+leftwristX = 0;
+leftwristy = 0;
+rightwristx = 0;
+rightwristy =0;
+
+function preload() {
+ leftmusic = loadSound("frozen.mp3")
+ rightmusic = loadSound("unstoppable.mp3")
+}
+
+function setup() {
+	canvas =  createCanvas(600, 500)
+	canvas.center()
+
+    video = createCapture(VIDEO)
+    video.hide()
+    
+    poseNet = ml5.poseNet(video, modeloaded)
+    poseNet.on("pose" , gotposes)
+}
+
+
+function modeloaded() {
+    console.log("model loaded")
+    status = true
+  }
+
+function draw() {
+    image(video,0,0,600,500)
+    fill ("#FF0000");
+    stroke ("#FF0000");
+
+    status1 = leftmusic.isPlaying();
+    status2 = rightmusic.isPlaying();
+  
+    if(rightwristscore > 0.2)
+	{ 
+		circle(rightwristx,rightwristy,20);
+
+			leftmusic.stop();
+
+		if(status2 == false)
+		{
+			rightmusic.play();
+		}
+	}
+}
+
+
+
+function gotposes(results) {
+    if(results.length > 0){
+        console.log(results)
+        leftwristx = results[0].pose.leftWrist.x
+        leftwristy = results[0].pose.leftWrist.y
+   
+        rightwristx = results[0].pose.rightWrist.x
+        rightwristy = results[0].pose.rightWrist.y
+        leftwristscore = results[0].pose.keypoint[9].score
+        rightwristscore = results[0].pose.keypoint[10].score
+    }
+}
